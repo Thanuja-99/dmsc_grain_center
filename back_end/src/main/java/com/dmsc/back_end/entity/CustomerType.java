e0f62bcdcce20dc508af5ed7b8c211bdf2e630fb
Thanuja-99 <thanujakumarasekara@gmail.com>
2026-05-24 12:46:17 +0530

    update customerType

--- back_end/src/main/java/com/dmsc/back_end/entity/CustomerType.java
+++ back_end/src/main/java/com/dmsc/back_end/entity/CustomerType.java
@@ -2,6 +2,7 @@ package com.dmsc.back_end.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,11 +18,24 @@ public class CustomerType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_type_id")
     private int customerTypeId;
+
+    @Column(name = "customer_type_name")
     private String customerTypeName;
+
+    @Column(name = "is_active")
     private boolean isActive;
+
+    @Column(name = "entered_by")
     private String enteredBy;
+
+    @Column(name = "entered_date")
     private LocalDate enteredDate;
+
+    @Column(name = "update_by")
     private String updateBy;
+
+    @Column(name = "update_date")
     private LocalDate updateDate;
 }
