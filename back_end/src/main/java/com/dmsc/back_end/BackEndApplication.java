d14817ae70f87aa7a6667593d7a3af5be25c6928
Thanuja-99 <thanujakumarasekara@gmail.com>
2026-05-24 12:46:32 +0530

    update main class

--- back_end/src/main/java/com/dmsc/back_end/BackEndApplication.java
+++ back_end/src/main/java/com/dmsc/back_end/BackEndApplication.java
@@ -3,7 +3,8 @@ package com.dmsc.back_end;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = "com.dmsc.back_end")
 public class BackEndApplication {
 
 	public static void main(String[] args) {
