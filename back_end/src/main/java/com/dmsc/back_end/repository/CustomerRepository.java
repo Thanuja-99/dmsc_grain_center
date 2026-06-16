4eff6d3f4c0f977001e0682f21d4e4f2665063fa
Thanuja-99 <thanujakumarasekara@gmail.com>
2026-06-16 07:06:42 +0530

    Customer Repository change

--- back_end/src/main/java/com/dmsc/back_end/repository/CustomerRepository.java
+++ back_end/src/main/java/com/dmsc/back_end/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.dmsc.back_end.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dmsc.back_end.entity.Customer;
@@ -7,6 +9,8 @@ import com.dmsc.back_end.entity.Customer;
 
 public interface CustomerRepository 
     extends JpaRepository<Customer, Integer> {
+
+    List<Customer> findByCallingNameContainingIgnoreCase(String name);
         
     }
 
